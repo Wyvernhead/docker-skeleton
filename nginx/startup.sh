c90e9c286ddbf172ce5d506bff1836df67f488e9
Yurii Movchan <movchan@logema.org>
2023-05-18 16:06:48 +0400

    Patched nginx to work from root but with proper rights for cert files

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -5,6 +5,7 @@ if [ ! -f /etc/nginx/ssl/default.crt ]; then
     openssl req -new -key "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.csr" -subj "/CN=default/O=default/C=UK"
     openssl x509 -req -days 365 -in "/etc/nginx/ssl/default.csr" -signkey "/etc/nginx/ssl/default.key" -out "/etc/nginx/ssl/default.crt"
     chmod 644 /etc/nginx/ssl/default.key
+    chown -R 1000:1000 /etc/nginx/ssl/
 fi
 
 # Start crond in background

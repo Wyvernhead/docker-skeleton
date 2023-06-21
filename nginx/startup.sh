225404b4cb0571ba2eae3e45b902ed87558b238a
Yurii Movchan <movchan@logema.org>
2023-06-21 14:41:39 +0400

    Теперь проверяем файл let's encrypt в директории архив, ибо в live лежат символические ссылки
    Signed-off-by: Yurii Movchan <movchan@logema.org>

--- nginx/startup.sh
+++ nginx/startup.sh
@@ -2,8 +2,10 @@
 
 if [ -n $DOMAIN ] && [ ! $DOMAIN = localhost ] && [ ! -f /etc/letsencrypt/archive/$DOMAIN/fullchain.pem ]; then
     certbot certonly --nginx -d $DOMAIN --noninteractive --agree-tos
-    chmod -R 644 /etc/letsencrypt/live/$DOMAIN/
     chown -R 1000:1000 /etc/letsencrypt/live/$DOMAIN/
+    chown -R 1000:1000 /etc/letsencrypt/archive/$DOMAIN/
+    chmod -R 644 /etc/letsencrypt/live/$DOMAIN/
+    chmod -R 644 /etc/letsencrypt/archive/$DOMAIN/
 fi
 
 # Start crond in background
